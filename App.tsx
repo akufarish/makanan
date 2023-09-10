0783d75c69b02b511ff68c52ac674012110898d0
akufarish <risfarishk@gmail.com>
2023-09-10 10:33:35 +0800

    rn

---
+++ App.tsx
@@ -0,0 +1,95 @@
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  */
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import React from 'react';
+// import type {PropsWithChildren} from 'react';
+// import {
+//   SafeAreaView,
+//   ScrollView,
+//   StatusBar,
+//   Text,
+//   useColorScheme,
+//   View,
+// } from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Text, View} from 'react-native';
+import Dashboard from './app/page';
+import Login from './app/auth/Login';
+
+// import {
+//   DebugInstructions,
+//   Header,
+//   LearnMoreLinks,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
+
+// type SectionProps = PropsWithChildren<{
+//   title: string;
+// }>;
+
+// function Section({children, title}: SectionProps): JSX.Element {
+//   return (
+//     <View className="mt-8 px-2">
+//       <Text className="text-2xl text-black dark:text-white">{title}</Text>
+//       <Text className="mt-2 text-lg text-black dark:text-white">
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// }
+
+// function App(): JSX.Element {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   const backgroundStyle = 'bg-neutral-300 dark:bg-slate-900';
+
+//   return (
+//     <SafeAreaView className={backgroundStyle}>
+//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+//       <ScrollView
+//         contentInsetAdjustmentBehavior="automatic"
+//         className={backgroundStyle}>
+//         <Header />
+//         <View className="bg-white dark:bg-black">
+//           <Section title="Step One">
+//             Edit <Text className="font-bold text-indigo-500">App.tsx</Text> to
+//             change this screen and then come back to see your edits.
+//           </Section>
+//           <Section title="See Your Changes">
+//             <ReloadInstructions />
+//           </Section>
+//           <Section title="Debug">
+//             <DebugInstructions />
+//           </Section>
+//           <Section title="Learn More">
+//             Read the docs to discover what to do next:
+//           </Section>
+//           <LearnMoreLinks />
+//         </View>
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// }
+
+// export default App;
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Dashboard" component={Dashboard}></Stack.Screen>
+          <Stack.Screen name="Login" component={Login}></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+export default function Dashboard() {
+  return (
+    <>
+      <View>
+        <Text className="text-red-500">ini dashboard</Text>
+      </View>
+    </>
+  );
+}
